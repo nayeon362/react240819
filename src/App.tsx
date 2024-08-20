4b9e0c2d1c74bd6ee4b0f147198edd4041799aff
nanayeon <tires0301@gmail.com>
2024-08-20 14:18:42 +0900

    12:50

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import Component, { FunctionComponent } from './component/Component';
 import './App.css';
 import CurlyBraces from './component/CurlyBraces';
 import Properties from './component_manage/Properties';
+import ConditionalRender from './component_manage/ConditionalRender';
+import Example2 from './component_manage/example/Example2';
 
 function App() {
   return (
     <>
-      {/*<Component /> */}
-      {/*<FunctionComponent />*/}
-      {/*<CurlyBraces />*/}
-      <Properties />
+      {/* <Component /> */}
+      {/* <FunctionComponent /> */}
+      {/* <CurlyBraces /> */}
+      {/* <Properties /> */}
+      {/* <ConditionalRender /> */}
+      <Example2 />
     </>
   );
 }
