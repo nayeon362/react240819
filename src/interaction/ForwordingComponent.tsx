0fe8ec4f2aec89fd1b77dc6aa3ee565524f9b2e3
nanayeon <tires0301@gmail.com>
2024-08-23 12:14:38 +0900

    12:50

---
+++ src/interaction/ForwordingComponent.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+
+// 컴포넌트로 상태 전달 :
+// - 부모요소에서 자식요소로 상태를 전달할 수 있음
+// - 자식 요소에 속성으로 전달
+// - 문제점 : 
+// - 자식 컴포넌트에서 상태를 변경하면 부모 컴포넌트가 리렌더링 됨
+// - 부모 컴포넌트의 자식 컴포넌트 중 해당 상태를 사용하지 않는  컴포넌트까지 리렌더링됨
+// - 코드의 복잡성 (컴포넌트의 속성 복잡성)이 증가
+// - 이 문제를 해결하기 위해 글로벌 상태로 관리하는 방법이 파생됨
+// - 글로벌 상태 관리 기법으로 context, Redux, zustand라는 기법이 존재
+
+export default function ForwordingComponent() {
+
+    const [count, setCount] = useState<number>(0);
+
+    const onIncrease = () => {
+        setCount(count => count + 1);
+    };
+
+    return (
+        <div>
+            <Child1 count={count} />
+            <Child2 />
+            <Child3 onIncrease={onIncrease} />
+        </div>
+    )
+
+    function Child1({ count }: Child1Props) {
+        return (
+            <div style={{ height: '200px', backgroundColor: '#ff000050' }}>
+                <h1>{count}</h1>
+            </div>
+
+
+        )
+    }
+
+    function Child2() {
+        return (
+            <div style={{ height: '200px', backgroundColor: '#00ff0050' }}></div>
+        )
+    }
+
+    interface Child3Props {
+        // count: number;
+        // setCount: React.Dispatch<React.SetStateAction<number>>
+        onIncrease: () => void;
+    }
+
+    function Child3({ /*count, setCount*/ onIncrease }: Child3Props) {
+
+        // const onIncrease = () => {
+        //     // setCount(count + 1);
+        //     setCount(count => count + 1);
+        // };
+
+        return (
+            <div style={{ height: '200px', backgroundColor: '#0000ff50' }}>
+                <button style={{ width: '100%', height: '100%' }} onClick={onIncrease}>+</button>
+            </div>
+        )
+    }
+}
--- src/interaction/StateComponent.tsx
+++ src/interaction/StateComponent.tsx
@@ -1,32 +1,107 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
-// 상태 (state) :
+// 상태(state)
 // - 각각의 컴포넌트가 독립적으로 가지고 있는 데이터 저장 공간
 // - 상태가 컴포넌트의 렌더링에 영향을 미침
-// - 상태가 변경되면 컴포넌트가 리렌더링 됨
 
 export default function StateComponent() {
 
     // state 선언 방법
     // - useState 훅 함수로 상태를 선언
-    // - count [상태변수, 상태변경함수] = useState<상태변수타입>(초기값);
+    // - const[상태변수, 상태변경함수] = useState<상태변수타입>(초기값);
+
     // let count: number = 0;
     const [count, setCount] = useState<number>(0);
-
-    // let counts: number[] = [];
+    const [total, setTotal] = useState<number>(0);
+    // let counts: number [] = [];
     const [counts, setCounts] = useState<number[]>([]);
 
-    const onIncrease = () => {
-        //setCount(count + 1);
+    const [comment, setComment] = useState<string>('');
 
-        // 상태 변수는 반드시 상태변경함수를 통해서 변경해야 리렌더링됨
+    let comm = '호호호';
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        comm = event.target.value;
+        console.log(comm);
+    }
+    const onIncrease = () => {
+        // setCount(count + 1);
+
+        // 상태 변수는 반드시 상태변경함수를 통해서 변경해야 리렌더링 됨
         // count++;
         // console.log(count);
-    };
+
+        // 상태변경함수를 사용하여 상태를 변경한다고 바로 변경되지 않음
+        // 함수가 끝나고 렌더링 되는 시점에 적용됨
+        setCount(count + 1);
+        setCount(count + 2);
+        setCount(count + 3);
+
+        // 상태변경함수에 콜백함수를 전달하면 해당 콜백 함수는 누적되어 실행됨
+        // 상태 count : 0
+        // argCount : 0
+        console.log('상태 count: ' + count)
+        setCount(argCount => {
+            console.log('argCount: ' + argCount)
+            return argCount + 1
+        })
+
+        // 상태 count : 0
+        // argCount : 1
+        console.log('상태 count: ' + count)
+        setCount(argCount => {
+            console.log('argCount: ' + argCount)
+            return argCount + 2
+        })
+
+        // 상태 count: 0
+        // argCount : 3
+        console.log('상태 count: ' + count)
+        setCount(argCount => {
+            console.log('argCount: ' + argCount)
+            return argCount + 3
+        })
+
+        // 상태 변경하는 작업이 한번에 여러 번 실행된다면 임시변수 활용하여 사용할 수 있음
+        // let tmp = 0;
+        // tmp = count;
+        // tmp += 1;
+        // tmp += 2;
+        // tmp += 3;
+        // setCount(tmp);
+
+        // 아래 코드는 setTotal에서 기존의 count 값을 사용하기 때문에
+        // 동작이 한단계 전 단계로 동작함
+        setCount(count + 1)
+        setTotal(total + count)
+
+        // 임시변수를 사용하여 변경 결과 값을 미리 저장하고 사용하면
+        // 위 코드의 문제를 해결할 수 있음
+        const newCount = count + 1;
+        setCount(newCount)
+        setTotal(total + newCount)
+
+
+        // 아래 코드는 배열에 요소를 추가했지만
+        // 실제 배열 주소가 바뀌지 않았기 때문에 변경을 인식 못함
+        // counts.push(newCount)
+        // console.log(counts)
+        // setCounts(counts)
+
+        // 타입이 배열 혹은 객체 형태인 상태는
+        // 반드시 새로운 배열 혹은 객체를 생성하고 변경해야 인식함
+        const newCounts = [...counts, newCount]
+        setCounts(newCounts)
+    }
 
     return (
         <div>
-            <h1>{count}</h1>
+            <h1>{comm}</h1>
+            <h1>{comment}</h1>
+            {/* 만약 input으로 상태를 변경한다면 value로 그 상태를 지정해야 불일치가 발생하지 않음 */}
+            <input value={comment} onChange={onChangeHandler} />
+            <h1>count : {count}</h1>
+            <h1>total : {total}</h1>
+            <h1>counts length: {counts.length} {counts}</h1>
             <button onClick={onIncrease}>+</button>
         </div>
     )
